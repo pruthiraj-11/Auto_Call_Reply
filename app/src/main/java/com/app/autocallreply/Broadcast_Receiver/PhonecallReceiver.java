11ae3a78f04f8cea063fc8ebf1062011d7cfb103
Pruthiraj Routaray <113031912+pruthiraj-74@users.noreply.github.com>
2023-11-17 19:57:24 +0530

    AC

--- app/src/main/java/com/app/autocallreply/Broadcast_Receiver/PhonecallReceiver.java
+++ app/src/main/java/com/app/autocallreply/Broadcast_Receiver/PhonecallReceiver.java
@@ -1,8 +1,12 @@
 package com.app.autocallreply.Broadcast_Receiver;
 
+
+import static android.content.Context.MODE_PRIVATE;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.telecom.TelecomManager;
 import android.telephony.SmsManager;
@@ -12,12 +16,19 @@ import android.widget.Toast;
 import androidx.core.app.ActivityCompat;
 
 import com.app.autocallreply.Activities.MainActivity;
+import com.app.autocallreply.Models.NumberModel;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class PhonecallReceiver extends BroadcastReceiver {
 
     Context context;
+    SharedPreferences sharedPreferences;
+    ArrayList<NumberModel> list;
     @Override
     public void onReceive(Context context, Intent intent) {
         this.context=context;
@@ -29,9 +40,20 @@ public class PhonecallReceiver extends BroadcastReceiver {
                 if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.ANSWER_PHONE_CALLS) != PackageManager.PERMISSION_GRANTED) {
                     return;
                 }
-                tm.endCall();
-                Toast.makeText(context, "Message Sent", Toast.LENGTH_SHORT).show();
-                sendSMS(savedNumber,"Busy");
+                list=new ArrayList<>();
+                sharedPreferences = context.getSharedPreferences("appInfo", MODE_PRIVATE);
+                Gson gson = new Gson();
+                String json = sharedPreferences.getString("numberList", null);
+                Type type = new TypeToken<ArrayList<NumberModel>>() {}.getType();
+                list= gson.fromJson(json, type);
+                if (list != null && !list.isEmpty()) {
+                    for (NumberModel numberModel:list){
+                        if (numberModel.getNumber().equals(savedNumber)){
+                            tm.endCall();
+                            sendSMS(savedNumber,"Busy");
+                        }
+                    }
+                }
             }
 //            if (savedNumber !=null){
 //                Intent intent1=new Intent(context, MainActivity.class);
